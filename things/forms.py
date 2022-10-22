8997647023f725b5a62869fd029302dc89dca114
Chenhao Liu <K21077228@kcl.ac.uk>
2022-10-22 15:18:38 +0100

    add form base

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
+from .models import Thing
+from django import forms
 
-# Create your forms here.
+
+class Form(forms.ModelForm):
+
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea, 'quantity': forms.NumberInput}
+
+
